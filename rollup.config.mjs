16d6ee6fc21f062844d56750ecb1b942955a93f1
Z0057393 <z0057393@protonmail.com>
2025-03-23 17:36:12 +0100

    Refacto

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,8 +1,23 @@
-// rollup.config.mjs
+// Import des plugins Rollup nécessaires
+import resolve from "@rollup/plugin-node-resolve"; // Pour résoudre les modules
+import commonjs from "@rollup/plugin-commonjs"; // Pour supporter les modules CommonJS
+import babel from "@rollup/plugin-babel"; // Pour transpiler le code avec Babel
+import { terser } from "rollup-plugin-terser"; // Pour minifier le code
+
 export default {
-  input: "src/content.js",
+  input: "src/01-Presentation/content.js",
   output: {
     file: "dist/bundle.js",
-    format: "cjs",
+    format: "iife",
+    name: "MyExtension",
   },
+  plugins: [
+    resolve(),
+    commonjs(),
+    babel({
+      babelHelpers: "bundled",
+      presets: ["@babel/preset-env"],
+    }),
+    terser(),
+  ],
 };
---
+++ src/02-Infrastructure/wordsManager.js
@@ -0,0 +1,9 @@
+export default class WordsManager {
+  constructor() {
+    this.words = ["interdit", "danger", "bloqué", "censuré"];
+  }
+
+  getWords() {
+    return this.words;
+  }
+}
