bf1959d6de53d935798ab2a70473186fa0f21940
Z0057393 <z0057393@protonmail.com>
2025-04-21 18:58:51 +0200

    Gérer les changement de channels

--- src/01-Presentation/content.js
+++ src/01-Presentation/content.js
@@ -2,3 +2,14 @@ import WatcherManager from "../03-Application/watcherManager.js";
 
 const watcher = new WatcherManager();
 watcher.start();
+
+let lastUrl = window.location.href;
+
+setInterval(() => {
+  const currentUrl = window.location.href;
+
+  if (currentUrl !== lastUrl) {
+    lastUrl = currentUrl;
+    watcher.start();
+  }
+}, 500);
